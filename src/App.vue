7656166f03aecf82fc80c859e3720edf3f5bd903
crystalpeng <crystalpeng1998@gmail.com>
2021-10-19 15:47:32 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <LULU msg="Hello Vue 3.0 + Vite" />
+</template>
+
+<script>
+import LULU from "./components/LULU.vue";
+
+export default {
+  name: "App",
+  components: {
+    LULU,
+  },
+};
+</script>
