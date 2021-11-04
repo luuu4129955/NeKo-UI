8b0bb412ad6123bce56b8fb7389b6e7a0fa678f9
crystalpeng <crystalpeng1998@gmail.com>
2021-11-04 22:31:44 +0800

    dialog 支持 visible

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -1,11 +1,21 @@
 <template>
-  <div></div>
-  <DialogD></DialogD>
+  <div>DialogD 组件 示例</div>
+  <ButtonD @click="toggle">toggle</ButtonD>
+  <DialogD :visible="x"></DialogD>
 </template>
 
 <script lang="ts">
 import DialogD from "../lib/DialogD.vue";
+import ButtonD from "../lib/ButtonD.vue";
+import { ref } from "vue";
 export default {
-  components: { DialogD },
+  components: { DialogD, ButtonD },
+  setup() {
+    const x = ref(false);
+    const toggle = () => {
+      x.value = !x.value;
+    };
+    return { x, toggle };
+  },
 };
 </script>
