9b4c77248bec27188346eec197caa0aa00759418
crystalpeng <crystalpeng1998@gmail.com>
2021-11-07 22:28:19 +0800

    优化代码

--- src/lib/TabsD.vue
+++ src/lib/TabsD.vue
@@ -6,7 +6,7 @@
         v-for="(t, index) in titles"
         :ref="
           (el) => {
-            if (el) navItems[index] = el;
+            if (t === selected) selectedItem = el;
           }
         "
         @click="select(t)"
@@ -30,7 +30,7 @@
 </template>
 
 <script lang="ts">
-import { computed, onMounted, onUpdated, ref } from "vue";
+import { computed, onMounted, onUpdated, ref, watchEffect } from "vue";
 import TabD from "./TabD.vue";
 export default {
   props: {
@@ -38,23 +38,19 @@ export default {
     selected: String,
   },
   setup(props, context) {
-    const navItems = ref<HTMLDivElement[]>([]);
+    const selectedItem = ref<HTMLDivElement>(null);
     const indicator = ref<HTMLDivElement>(null);
     const container = ref<HTMLDivElement>(null);
     const x = () => {
-      const divs = navItems.value;
-      const result = divs.filter((div) =>
-        div.classList.contains("selected")
-      )[0];
-      const { width } = result.getBoundingClientRect();
+      const { width } = selectedItem.value.getBoundingClientRect();
       indicator.value.style.width = width + "px";
       const { left: left1 } = container.value.getBoundingClientRect();
-      const { left: left2 } = result.getBoundingClientRect();
+      const { left: left2 } = selectedItem.value.getBoundingClientRect();
       const left = left2 - left1;
       indicator.value.style.left = left + "px";
     };
-    onMounted(x);
-    onUpdated(x);
+
+    watchEffect(x); //onMounted(x);第一次 + onUpdated(x)后面的;
     const defaults = context.slots.default();
     defaults.forEach((tag) => {
       if (tag.type !== TabD) {
@@ -64,23 +60,17 @@ export default {
     const titles = defaults.map((tag) => {
       return tag.props.title;
     });
-    const current = computed(() => {
-      console.log("重新 return");
-      return defaults.filter((tag) => {
-        return tag.props.title === props.selected;
-      })[0];
-    });
+
     const select = (title: string) => {
       context.emit("update:selected", title);
     };
     return {
       defaults,
       titles,
-      current,
       select,
-      navItems,
       indicator,
       container,
+      selectedItem,
     };
   },
 };
